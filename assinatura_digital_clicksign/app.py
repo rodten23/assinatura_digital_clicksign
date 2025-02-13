7a1501bd7c5f0fbd40fc4adc153d2fc5b50a59b5
Rodrigo Alves Tenório <rodten23@gmail.com>
2025-02-13 10:10:38 -0300

    :tada: Primeiro commit.

---
+++ assinatura_digital_clicksign/app.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+host = 'https://sandbox.clicksign.com'
+headers = {'Content-Type': 'application/json',
+           'Accept': 'application/json',
+           'Connection': 'keep-alive',
+           'access_token': '7e9dc98f-3e49-4d90-a7f1-ada90f427b01',
+           'doc_key': '10714765-2578-41c8-91cb-13336fda9be5'}
+
+params = 'access_token=7e9dc98f-3e49-4d90-a7f1-ada90f427b01'
+doc_key = '10714765-2578-41c8-91cb-13336fda9be5'
+
+payload = {
+    "document": {
+        "path": "/rod/rodrigo.pdf",
+        "template": {
+            "data": ''
+        }
+    }
+}
+
+criacao_termo_url = f'https://sandbox.clicksign.com/api/v1/templates/{doc_key}/documents?{params} HTTP/1.1'
+
+print(criacao_termo_url)
+#response = requests.post(url=criacao_termo_url, json=payload, headers=headers)
+
+response = requests.post(url=criacao_termo_url, json=payload, headers=headers)
+print(response.url)
+print(response.status_code)
+# if __name__ == '__main__':
+#     app.run(debug=True)
