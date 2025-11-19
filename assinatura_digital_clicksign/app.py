9f8b6a54526ee025e01504cbe7a4d95de1cd9ec9
Rodrigo Alves Tenorio <rodrigo.atenorio@telefonica.com>
2025-11-19 14:45:55 -0300

    :sparkles: feat: Desenvolvimento geral da solução funcional backend usando FastAPI.

--- assinatura_digital_clicksign/app.py
+++ assinatura_digital_clicksign/app.py
@@ -1,32 +1,144 @@
-import requests
 import json
+import os
 
-host = 'https://sandbox.clicksign.com'
-headers = {'Content-Type': 'application/json',
-           'Accept': 'application/json',
-           'Connection': 'keep-alive',
-           'access_token': '7e9dc98f-3e49-4d90-a7f1-ada90f427b01',
-           'doc_key': '10714765-2578-41c8-91cb-13336fda9be5'}
+import httpx
+from dotenv import load_dotenv
 
-params = 'access_token=7e9dc98f-3e49-4d90-a7f1-ada90f427b01'
-doc_key = '10714765-2578-41c8-91cb-13336fda9be5'
+from fastapi import FastAPI, Depends
 
-payload = {
-    "document": {
-        "path": "/rod/rodrigo.pdf",
-        "template": {
-            "data": ''
-        }
-    }
+app = FastAPI()
+
+load_dotenv()
+
+base_url = os.getenv('BASE_URL')
+access_token = os.getenv('ACCESS_TOKEN')
+doc_key = os.getenv('DOC_KEY')
+
+headers = {
+    'Host': 'sandbox.clicksign.com',
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
 }
 
-criacao_termo_url = f'https://sandbox.clicksign.com/api/v1/templates/{doc_key}/documents?{params} HTTP/1.1'
+@app.post('/criar_signatario')
+def creation_signer() -> str:
+    
+    creation_signer_url = f'{base_url}/signers?access_token={access_token}'
 
-print(criacao_termo_url)
-#response = requests.post(url=criacao_termo_url, json=payload, headers=headers)
+    signer_data = json.dumps({
+        'signer': {
+            'email': 'testador@testador.com',
+            'phone_number': '11988776655',
+            'auths': [
+                'sms'
+            ],
+            'name': 'Testador Que Assina',
+            'documentation': '123.480.920-69',
+            'communicate_by': 'email',
+            'has_documentation': 'true',
+            'selfie_enabled': 'false',
+            'handwritten_enabled': 'false',
+            'location_required_enabled': 'true',
+            'official_document_enabled': 'false',
+            'liveness_enabled': 'false',
+            'facial_biometrics_enabled': 'false'
+        }
+    })
 
-response = requests.post(url=criacao_termo_url, json=payload, headers=headers)
-print(response.url)
-print(response.status_code)
-# if __name__ == '__main__':
-#     app.run(debug=True)
+    signer_response = httpx.post(
+        url=creation_signer_url,
+        data=signer_data,
+        headers=headers,
+        verify=False
+    )
+
+    with open(
+        './assinatura_digital_clicksign/signer_response.json',
+        'w', encoding='utf-8') as open_file:
+        json.dump(
+            signer_response.json(),
+            open_file,
+            ensure_ascii=False,
+            indent=4
+        )
+
+    signer_key = signer_response.json()['signer']['key']
+
+    return signer_key
+
+@app.post('/criar_contrato')
+def creation_contract() -> str:
+    
+    creation_contract_url = f'{base_url}/templates/{doc_key}/documents?access_token={access_token}'
+
+    contract_data = json.dumps({
+        'document': {
+            'path': '/CONTRATOS-TESTES/TIC_YYYYMMDDhhmmss_22233344455.pdf',
+            'template': {
+                'data': {
+                    'idDiretorio': '1234567',
+                    'nome': 'TIC_YYYYMMDD_CPFgestorQueConvidou',
+                    'descricao': 'Termo de inclusão',
+                    'dataValidade': '',
+                    'extensao': 'pdf',
+                    'tipoArquivo': 293
+          }
+        }
+      }
+    })
+
+    contract_response = httpx.post(
+        url=creation_contract_url,
+        data=contract_data,
+        headers=headers,
+        verify=False
+    )
+
+    with open(
+        './assinatura_digital_clicksign/contract_response.json',
+        'w', encoding='utf-8') as open_file:
+        json.dump(
+            contract_response.json(),
+            open_file,
+            ensure_ascii=False,
+            indent=4
+        )
+
+    contract_key = contract_response.json()['document']['key']
+
+    return contract_key
+
+@app.post('/relacionar_contrato')
+def creation_contract(signer_key: str = Depends(creation_signer), contract_key: str = Depends(creation_contract)):
+    
+    relate_contract_url = f'{base_url}/lists?access_token={access_token}'
+
+    relate_contract_data = json.dumps({
+        'list': {
+            'document_key': contract_key,
+            'signer_key': signer_key,
+            'sign_as': 'administrator',
+            'refusable': 'true'
+        }
+    })
+
+    relate_contract_response = httpx.post(
+        url=relate_contract_url,
+        data=relate_contract_data,
+        headers=headers,
+        verify=False
+    )
+
+    with open(
+        './assinatura_digital_clicksign/relate_contract_response.json',
+        'w', encoding='utf-8') as open_file:
+        json.dump(
+            relate_contract_response.json(),
+            open_file,
+            ensure_ascii=False,
+            indent=4
+        )
+
+    relate_contract_url = relate_contract_response.json()['list']['url']
+
+    return relate_contract_url
