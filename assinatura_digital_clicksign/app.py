760690300f7d333fa87d3cf6eee2efb32d60354c
Rodrigo Alves Tenório <rodten23@gmail.com>
2026-07-25 00:16:44 -0300

    :sparkles: feat: Inclusão do código para qualificar documento com signatário na Clicksign e ajustes em todas as chamadas.

--- assinatura_digital_clicksign/app.py
+++ assinatura_digital_clicksign/app.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from pathlib import Path
+
 from datetime import date
 
 import httpx
@@ -87,6 +89,10 @@ def criar_envelope() -> str:
             indent=4
         )
 
+    return ler_envelope()
+
+
+def ler_envelope():
     with open(
         './assinatura_digital_clicksign/resposta_envelope.json',
         'r',encoding='utf-8') as open_file:
@@ -96,8 +102,19 @@ def criar_envelope() -> str:
     
     return chave_envelope
 
+
+
 @app.post('/criar_signatario')
-def criar_signatario(chave_envelope=criar_envelope()) -> str:
+def criar_signatario(envelope=Path('./assinatura_digital_clicksign/resposta_envelope.json')) -> str:
+
+    chave_envelope = ''
+
+    if envelope.is_file():
+        chave_envelope = ler_envelope()
+        
+    else:
+        chave_envelope = criar_envelope()
+        
     
     criar_signatario_url = f'{base_url}/envelopes/{chave_envelope}/signers'
 
@@ -156,6 +173,9 @@ def criar_signatario(chave_envelope=criar_envelope()) -> str:
             indent=4
         )
 
+    return ler_signatario()
+
+def ler_signatario():
     with open(
         './assinatura_digital_clicksign/resposta_signatario.json',
         'r',encoding='utf-8') as open_file:
@@ -169,9 +189,28 @@ def criar_signatario(chave_envelope=criar_envelope()) -> str:
 
 
 @app.post('/criar_documento')
-def criar_documento(nome_signatario=criar_signatario()['nome'],
-                    documento_signatario = criar_signatario()['documento'],
-                    chave_envelope=criar_envelope()) -> str:
+def criar_documento(
+    envelope=Path('./assinatura_digital_clicksign/resposta_envelope.json'),signatario=Path('./assinatura_digital_clicksign/resposta_signatario.json')) -> str:
+
+    chave_envelope = ''
+    nome_signatario = ''
+    documento_signatario = ''
+
+    if envelope.is_file():
+        chave_envelope = ler_envelope()
+        
+    else:
+        chave_envelope = criar_envelope()
+
+
+    if signatario.is_file():
+        nome_signatario = ler_signatario()['nome']
+        documento_signatario = ler_signatario()['documento']
+        
+    else:
+        nome_signatario = criar_signatario()['nome']
+        documento_signatario = criar_signatario()['documento']
+    
     
     criar_documento_url = f'{base_url}/envelopes/{chave_envelope}/documents'
 
@@ -217,6 +256,10 @@ def criar_documento(nome_signatario=criar_signatario()['nome'],
             indent=4
         )
 
+    return ler_documento()
+
+
+def ler_documento():
     with open(
         './assinatura_digital_clicksign/resposta_documento.json',
         'r',encoding='utf-8') as open_file:
@@ -227,79 +270,85 @@ def criar_documento(nome_signatario=criar_signatario()['nome'],
     return chave_documento
 
 
-# @app.post('/criar_contrato')
-# def creation_contract() -> str:
+@app.post('/qualificar_signatario_documento')
+def qualificar_sig_doc(
+    envelope=Path('./assinatura_digital_clicksign/resposta_envelope.json'),
+    documento=Path('./assinatura_digital_clicksign/resposta_documento.json'),
+    signatario=Path('./assinatura_digital_clicksign/resposta_signatario.json')) -> str:
     
-#     creation_contract_url = f'{base_url}/templates/{template_id}/documents?access_token={access_token}'
 
-#     contract_data = json.dumps({
-#         'document': {
-#             'path': '/CONTRATOS-TESTES/TIC_YYYYMMDDhhmmss_22233344455.pdf',
-#             'template': {
-#                 'data': {
-#                     'idDiretorio': '1234567',
-#                     'nome': 'TIC_YYYYMMDD_CPFgestorQueConvidou',
-#                     'descricao': 'Termo de inclusão',
-#                     'dataValidade': '',
-#                     'extensao': 'pdf',
-#                     'tipoArquivo': 293
-#           }
-#         }
-#       }
-#     })
-
-#     contract_response = httpx.post(
-#         url=creation_contract_url,
-#         data=contract_data,
-#         headers=headers,
-#         verify=False
-#     )
-
-#     with open(
-#         './assinatura_digital_clicksign/contract_response.json',
-#         'w', encoding='utf-8') as open_file:
-#         json.dump(
-#             contract_response.json(),
-#             open_file,
-#             ensure_ascii=False,
-#             indent=4
-#         )
-
-#     contract_key = contract_response.json()['document']['key']
-
-#     return contract_key
-
-# @app.post('/relacionar_contrato')
-# def creation_contract(signer_key: str = Depends(creation_signer), contract_key: str = Depends(creation_contract)):
+    chave_envelope = ''
+    chave_documento = ''
+    chave_signatario = ''
     
-#     relate_contract_url = f'{base_url}/lists?access_token={access_token}'
+    if envelope.is_file():
+        chave_envelope = ler_envelope()      
+    else:
+        chave_envelope = criar_envelope()
 
-#     relate_contract_data = json.dumps({
-#         'list': {
-#             'document_key': contract_key,
-#             'signer_key': signer_key,
-#             'sign_as': 'administrator',
-#             'refusable': 'true'
-#         }
-#     })
+    if documento.is_file():
+        chave_documento = ler_documento()                
+    else:
+        chave_documento = criar_documento()    
+    
+    if signatario.is_file():
+        chave_signatario = ler_signatario()['chave']            
+    else:
+        chave_signatario = criar_signatario()['chave']
 
-#     relate_contract_response = httpx.post(
-#         url=relate_contract_url,
-#         data=relate_contract_data,
-#         headers=headers,
-#         verify=False
-#     )
+    
+    qualificar_sig_doc_url = f'{base_url}/envelopes/{chave_envelope}/requirements'
 
-#     with open(
-#         './assinatura_digital_clicksign/relate_contract_response.json',
-#         'w', encoding='utf-8') as open_file:
-#         json.dump(
-#             relate_contract_response.json(),
-#             open_file,
-#             ensure_ascii=False,
-#             indent=4
-#         )
+    body_qualificacao = json.dumps({
+        'data': {
+            'type': 'requirements',
+            'attributes': {
+                "action": "agree",
+                "role": "sign"
+            },
+            'relationships': {
+                'document': {
+                    'data': {
+                        'type': 'documents',
+                        'id': chave_documento
+                    }
+                },
+                'signer': {
+                    'data': {
+                        'type': 'signers',
+                        'id': chave_signatario
+                    }
+                }
+            }
+        }
+    })
 
-#     relate_contract_url = relate_contract_response.json()['list']['url']
+    resposta_qualificar_sig_doc = httpx.post(
+        url=qualificar_sig_doc_url,
+        data=body_qualificacao,
+        headers=headers,
+        verify=False
+    )
 
-#     return relate_contract_url
+    with open(
+        './assinatura_digital_clicksign/resposta_qualificar_sig_doc.json',
+        'w', encoding='utf-8') as response_file:
+        json.dump(
+            resposta_qualificar_sig_doc.json(),
+            response_file,
+            ensure_ascii=False,
+            indent=4
+        )
+
+    return ler_qualificacao()
+
+
+def ler_qualificacao():
+    with open(
+        './assinatura_digital_clicksign/resposta_qualificar_sig_doc.json',
+        'r',encoding='utf-8') as open_file:
+            dados_qualificacao = json.load(open_file)
+        
+    chave_qualificacao = dados_qualificacao['data']['id']
+        
+    return chave_qualificacao
